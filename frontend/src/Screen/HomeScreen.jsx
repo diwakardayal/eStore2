798e028a068f5ed1c9d71caa459a78f2d19043c1
diwakardayal <tinku@dd.com>
2023-11-14 15:47:00 +0530

    add loader & custom message component;

--- frontend/src/Screen/HomeScreen.jsx
+++ frontend/src/Screen/HomeScreen.jsx
@@ -1,6 +1,8 @@
 import { useGetProductsQuery } from "../slices/productsApiSlice"
 import { Row, Col } from "react-bootstrap"
 import Product from "../components/Product"
+import Loader from "../components/Loader"
+import Message from "../components/Message"
 
 export default function HomeScreen() {
 	// eslint-disable-next-line no-unused-vars
@@ -10,15 +12,22 @@ export default function HomeScreen() {
 	return (
 		<>
 			<h1>Latest Products</h1>
-			<Row>
-				{products &&
-					products.length > 0 &&
-					products.map(product => (
-						<Col key={product._id} sm={12} md={6} lg={4} xl={3}>
-							<Product product={product} />
-						</Col>
-					))}
-			</Row>
+
+			{isLoading ? (
+				<Loader />
+			) : error ? (
+				<Message>{error?.data?.message || error.error}</Message>
+			) : (
+				<Row>
+					{products &&
+						products.length > 0 &&
+						products.map(product => (
+							<Col key={product._id} sm={12} md={6} lg={4} xl={3}>
+								<Product product={product} />
+							</Col>
+						))}
+				</Row>
+			)}
 		</>
 	)
 }
