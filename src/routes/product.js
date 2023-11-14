a094140b1894b6ad2e10f9c669597f5894b5d421
diwakardayal <tinku@dd.com>
2023-11-14 15:30:13 +0530

    Fix: product route error ERR_HTTP_HEADERS_SENT;

--- src/routes/product.js
+++ src/routes/product.js
@@ -13,15 +13,17 @@ router.get("/", async (req, res) => {
 })
 
 // Get product by id
+// eslint-disable-next-line consistent-return
 router.get("/:id", async (req, res) => {
 	try {
 		const product = await Product.findById(req.params.id)
 
 		if (product) {
-			res.json(product)
+			return res.json(product)
 		}
 
 		res.status(400).json({ message: "Product Not Found" })
+		throw new Error("Product not found")
 	} catch (e) {
 		console.log(e)
 	}
